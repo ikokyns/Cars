82801b91ffe6689d8996248af6d8978be5285f89
ikokyns <momcilo87@hotmail.com>
2017-08-29 18:41:58 +0200

    Zadatak 1

---
+++ src/app/components/cars/cars.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cars',
+  templateUrl: './cars.component.html'
+})
+
+export class CarsComponent implements OnInit {
+	
+	private cars: any = [];
+
+  constructor() {
+
+  }
+
+
+
+
+  ngOnInit() {
+  }
+
+}
